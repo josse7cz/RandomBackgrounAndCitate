a665a42eebc3d8878a06f0620cfe8aabbef70c2f
Josef Janda <josefjanda@email.cz>
2023-06-11 12:15:40 +0200

    založení projektu
    main

---
+++ src/main/java/com/example/randombackgrounandcitate/RandomBackgrounAndCitateApplication.java
@@ -0,0 +1,13 @@
+package com.example.randombackgrounandcitate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomBackgrounAndCitateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomBackgrounAndCitateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/randombackgrounandcitate/RandomBackgrounAndCitateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randombackgrounandcitate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomBackgrounAndCitateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
